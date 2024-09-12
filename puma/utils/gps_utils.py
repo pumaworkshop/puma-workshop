8ad6cd949bef1f5475d34b3c25684decdfc96a36
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2024-09-12 17:30:51 +0200

    Separate update calculation and appium location spoofing to 2 threads

--- puma/utils/gps_utils.py
+++ puma/utils/gps_utils.py
@@ -13,13 +13,16 @@ from gpxpy.gpx import GPXTrackPoint
 class GpsUtils:
 
     def __init__(self, driver: WebDriver):
-        self._current_speed = 80
-        self._next_point = None
-        self._point_updated = False
+        # static settings
+        self._current_speed = 90
+        self._location_update_interval = 1
+        # dynamic fields
         self.driver = driver
+        # fields relating to current and future locations
+        self._next_point = None
         self._current_location: Optional[Point] = None
         self._next_locations: [Point] = []
-        self._last_location_update = None
+        self._last_location_update: Optional[float] = None
 
     def _load_gpx(self, gpx_file: str):
         points: [GPXTrackPoint] = []
@@ -38,6 +41,7 @@ class GpsUtils:
         if not self._next_locations or distance_to_travel <= 0:
             return
         distance_to_next_point = geopy.distance.geodesic(self._current_location, self._next_locations[0]).m
+
         if distance_to_next_point < distance_to_travel:
             self._current_location = self._next_locations.pop(0)
             self._travel_distance(distance_to_travel - distance_to_next_point)
@@ -63,24 +67,41 @@ class GpsUtils:
             distance_to_travel = time_elapsed * ((self._current_speed * 1000) / 3600)  # km/h to m/s
             self._travel_distance(distance_to_travel)
         self._last_location_update = now
-        self.set_appium_location(self._current_location.latitude,
+
+    def _update_appium_location(self):
+        self.driver.set_location(self._current_location.latitude,
                                  self._current_location.longitude,
                                  self._current_location.altitude,
-                                 self._current_speed)
+                                 self._current_speed, 5)
 
     def _route_loop(self):
         while self._next_locations:
+            start = time.time()
             self._update_location()
-            sleep(1)
+            stop = time.time()
+            delay = stop - start
+            sleep(max(self._location_update_interval - delay, 0.1))
+
+    def _appium_loop(self):
+        while self._current_location:
+            start = time.time()
+            self._update_appium_location()
+            stop = time.time()
+            delay = stop - start
+            sleep(max(self._location_update_interval - delay, 0.1))
 
     def execute_route(self, gpx_file: str):
         self._load_gpx(gpx_file)
+        self.start_route()
+
+    def start_route(self):
+        # todo: make method to stop route, and stop these threads properly
         consumer_thread = threading.Thread(target=self._route_loop)
+        consumer_thread.daemon = True
+        consumer_thread.start()
+        consumer_thread = threading.Thread(target=self._appium_loop())
         consumer_thread.daemon = True
         consumer_thread.start()
 
     def update_speed(self, speed: float):
         self._current_speed = speed
-
-    def set_appium_location(self, lat: float, lon: float, alt: float = 0, speed: float = 0, num_sat: float = 0):
-        self.driver.set_location(lat, lon, alt, speed, num_sat)
