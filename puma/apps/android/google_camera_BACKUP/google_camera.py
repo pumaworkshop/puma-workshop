0f74bcdc8d55e53aa90e50adac3aaa993e2e1f41
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2024-09-20 09:17:19 +0200

    Add google camera backup for demos

---
+++ puma/apps/android/google_camera_BACKUP/google_camera.py
@@ -0,0 +1,44 @@
+import time
+from typing import Dict
+
+from appium.webdriver.common.appiumby import AppiumBy
+
+from puma.apps.android.appium_actions import AndroidAppiumActions
+
+GOOGLE_CAMERA_PACKAGE = 'com.google.android.GoogleCamera'
+GOOGLE_CAMERA_DEFAULT_ACTIVITY = ''
+
+
+class GoogleCameraActions(AndroidAppiumActions):
+    def __init__(self,
+                 device_udid,
+                 desired_capabilities: Dict[str, str] = None,
+                 implicit_wait=1,
+                 appium_server='http://localhost:4723'):
+        AndroidAppiumActions.__init__(self,
+                                      device_udid,
+                                      GOOGLE_CAMERA_PACKAGE,
+                                      GOOGLE_CAMERA_DEFAULT_ACTIVITY,
+                                      desired_capabilities=desired_capabilities,
+                                      implicit_wait=implicit_wait,
+                                      appium_server=appium_server)
+
+    def take_picture(self):
+        xpath = '//android.widget.ImageButton[@resource-id="com.google.android.GoogleCamera:id/shutter_button"]'
+        shutter = self.driver.find_element(by=AppiumBy.XPATH,value=xpath)
+        shutter.click()
+
+    def switch_camera(self):
+        xpath = '//android.widget.ImageButton[@resource-id="com.google.android.GoogleCamera:id/camera_switch_button"]'
+        button = self.driver.find_element(by=AppiumBy.XPATH, value=xpath)
+        button.click()
+
+if __name__ == '__main__':
+    with GoogleCameraActions('32131JEHN38079') as alice_camera:
+        alice_camera.switch_camera()
+        time.sleep(1)
+        alice_camera.take_picture()
+        time.sleep(1)
+        alice_camera.switch_camera()
+        time.sleep(1)
+        alice_camera.take_picture()
