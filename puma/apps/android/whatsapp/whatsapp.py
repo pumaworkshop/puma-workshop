0ebb57c99e14f3268990e5947d6e62f312aea449
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-01-16 16:03:29 +0100

    59 Whatsapp: Bump version to 2.24.25.78

--- puma/apps/android/whatsapp/whatsapp.py
+++ puma/apps/android/whatsapp/whatsapp.py
@@ -10,7 +10,7 @@ from puma.apps.android.appium_actions import supported_version, AndroidAppiumAct
 WHATSAPP_PACKAGE = 'com.whatsapp'
 
 
-@supported_version("2.24.23.78")
+@supported_version("2.24.25.78")
 class WhatsappActions(AndroidAppiumActions):
     def __init__(self,
                  device_udid,
