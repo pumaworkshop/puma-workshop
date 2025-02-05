fae76d9a88e628448d8e906313025823754d6e7f
Oudsen, Erik <eoudsen@users.noreply.github.com>
2025-02-05 15:00:40 +0100

    Add framework for exercise 4

---
+++ puma/apps/android/your_app/your_app.py
@@ -0,0 +1,23 @@
+from typing import Dict
+
+from puma.apps.android.appium_actions import AndroidAppiumActions, supported_version
+
+YOUR_APP_PACKAGE = 'com.your.app'
+
+@supported_version("1.0.0")
+class YourApp(AndroidAppiumActions):
+
+    def __init__(self,
+                 device_udid,
+                 desired_capabilities: Dict[str, str] = None,
+                 implicit_wait=1,
+                 appium_server='http://localhost:4723'):
+        AndroidAppiumActions.__init__(self,
+                                      device_udid,
+                                      YOUR_APP_PACKAGE,
+                                      desired_capabilities=desired_capabilities,
+                                      implicit_wait=implicit_wait,
+                                      appium_server=appium_server)
+
+    def your_app_function(self):
+        print("Functionality can be added here!")
