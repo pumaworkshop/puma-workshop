d8cccb902f44940ecb07d6175f0f75078b947b0e
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-03-19 09:17:41 +0100

    Improve node and appium install script

--- install/windows/install_node_appium.bat
+++ install/windows/install_node_appium.bat
@@ -29,9 +29,23 @@ IF %ERRORLEVEL% NEQ 0 (
     echo Node.js is already installed.
 )
 
-:: Install Appium using npm
-echo Installing Appium...
-npm install -g appium
-appium driver install uiautomator2
+:: Check if Appium is already installed
+npm list -g --depth=0 | findstr /C:"appium" >nul
+IF %ERRORLEVEL% NEQ 0 (
+    echo Appium is not installed. Installing Appium...
+    call npm install -g appium
+    echo Appium installed.
+) ELSE (
+    echo Appium is already installed.
+)
+
+:: Check if uiautomator2 driver is installed
+appium driver list --installed 2>&1 | findstr /C:"uiautomator2" >nul
+IF %ERRORLEVEL% NEQ 0 (
+    echo uiautomator2 driver is not installed. Installing uiautomator2 driver...
+    call appium driver install uiautomator2
+) ELSE (
+    echo uiautomator2 driver is already installed.
+)
 
 echo Appium installation completed.
