fa29457974d44a8a3309176c0063e852498154ad
Oudsen, Erik <eoudsen@users.noreply.github.com>
2025-03-19 12:19:23 +0100

    Merge scripts for nvm and node

--- install/linux/install_node_appium.sh
+++ install/linux/install_node_appium.sh
@@ -3,6 +3,14 @@
 # Exit on error
 set -e
 
+# Install NVM (Node Version Manager)
+echo "Installing NVM..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+
+# Source NVM to use it immediately
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 # Install Node.js v19
 echo "Installing Node.js v19..."
 nvm install 19
