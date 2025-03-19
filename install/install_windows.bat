7facb026c3fae8884a9230fa9b9c362307e11e43
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-03-19 09:17:27 +0100

    Add administrative rights check

--- install/install_windows.bat
+++ install/install_windows.bat
@@ -1,5 +1,13 @@
 @echo off
 
+:: Check for administrator rights
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo This script requires administrative privileges. Please run as administrator.
+    pause
+    exit /b 1
+)
+
 :: Initialize success flags
 set "git_success=false"
 set "python_success=false"
